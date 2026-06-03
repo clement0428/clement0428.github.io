eb03f012e29e91f4d56310d2db350cc4849668a6
Clement <clement0428@gmail.com>
2026-06-03 21:39:03 +0800

    Deploy O2O3 parser truthfulness fix

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '8efd74b',
-  deployed_at: '2026-06-03T19:20:00+08:00',
+  commit: '74ee7ff',
+  deployed_at: '2026-06-03T19:55:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
