fe6d46533985fe388425ae658cde520349c2173e
Clement <clement0428@gmail.com>
2026-06-03 22:20:40 +0800

    Restore O2O3 matrix bundle after additive deploy

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_BUILD_META__ = {
   commit: 'e0d1457',
-  deployed_at: '2026-06-03T22:15:41+08:00',
+  deployed_at: '2026-06-03T22:20:21+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
