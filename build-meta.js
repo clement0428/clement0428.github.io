5072f37f867bcac0bdaa254d95c56ea5f2db76cb
Clement <clement0428@gmail.com>
2026-06-03 18:18:57 +0800

    Deploy O2O3 position code severity parsing

--- build-meta.js
+++ build-meta.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'c179d49',
-  deployed_at: '2026-06-03T17:58:58+08:00',
+  commit: '2caf1ed',
+  deployed_at: '2026-06-03T18:18:56+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
