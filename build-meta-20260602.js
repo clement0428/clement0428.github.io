ee2a582796b71079f80dfba8f72d350546fbf55d
Clement <clement0428@gmail.com>
2026-06-03 08:25:43 +0800

    Deploy O2O3 heatmap without machine room

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "56cfaaf",
-  deployed_at: "2026-06-03T16:28:00+08:00"
+  commit: "ee60a7d",
+  deployed_at: "2026-06-03T16:40:00+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
