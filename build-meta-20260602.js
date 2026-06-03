d7d2a6c24c1f0e4807f9923d3405f51877d7414b
Clement <clement0428@gmail.com>
2026-06-03 17:58:59 +0800

    Deploy build metadata fix for L1-L5 O2O3

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '05a5dd7',
-  deployed_at: '2026-06-03T17:40:43+08:00',
+  commit: 'c179d49',
+  deployed_at: '2026-06-03T17:58:58+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
