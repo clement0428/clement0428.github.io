97a20daad91ec57bab00ae2bd2eaf4a778055f02
Clement <clement0428@gmail.com>
2026-06-03 08:15:35 +0800

    Deploy KengYi sentinel tabs build

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "7fe9bf4",
-  deployed_at: "2026-06-03T16:20:00+08:00"
+  commit: "56cfaaf",
+  deployed_at: "2026-06-03T16:28:00+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
