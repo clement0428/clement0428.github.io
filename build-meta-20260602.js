ede38ab662f49ffc535c7c49f358b64f0353c9c1
Clement <clement0428@gmail.com>
2026-06-03 10:48:04 +0800

    Deploy O2O3 photo upload fix

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "efdf99f",
-  deployed_at: "2026-06-03T17:25:00+08:00"
+  commit: "0986dcd",
+  deployed_at: "2026-06-03T18:46:00+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
