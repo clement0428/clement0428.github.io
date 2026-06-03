29d45316b5a8a06d9549d61637de8ced928376ab
Clement <clement0428@gmail.com>
2026-06-03 12:17:49 +0800

    Deploy O2O3 staged upload workflow

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "c64af31",
-  deployed_at: "2026-06-03T11:01:00+08:00"
+  commit: "bdf4e07",
+  deployed_at: "2026-06-03T12:15:30+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
