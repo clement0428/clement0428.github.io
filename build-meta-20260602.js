d8ff040f4788143c5923f2ec9074d15d57bae358
Clement <clement0428@gmail.com>
2026-06-03 18:59:40 +0800

    Deploy KengYi O2O3 outcome flow

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '2caf1ed',
-  deployed_at: '2026-06-03T18:18:56+08:00',
+  commit: '8efd74b',
+  deployed_at: '2026-06-03T19:20:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
