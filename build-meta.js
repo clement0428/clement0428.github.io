084a7a40926f2075115797d40009d21a69bdff91
Clement <clement0428@gmail.com>
2026-06-03 13:33:57 +0800

    Deploy real O2O3 floor plan heatmap

--- build-meta.js
+++ build-meta.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "4f428cd",
-  deployed_at: "2026-06-03T13:17:12+08:00"
+  commit: "5f1fda6",
+  deployed_at: "2026-06-03T13:33:30+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
 
