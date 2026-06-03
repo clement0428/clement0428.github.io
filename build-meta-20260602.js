7cc804e88580662da332837b7411fac22c114fcf
Clement <clement0428@gmail.com>
2026-06-04 07:50:13 +0800

    Deploy refreshed build stamp metadata

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '57e7eb8',
-  source_commit: '57e7eb8e6d1172e62961e34014728151b935a1c2',
-  deployed_at: '2026-06-04T00:12:00+08:00',
+  commit: '018bdd8',
+  source_commit: '018bdd87d2aae60935c120cd30b7413befb00f67',
+  deployed_at: '2026-06-04T00:50:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
