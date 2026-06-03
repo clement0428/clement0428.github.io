03f7c8b11315e0832eaee4776c394c46df10a702
Clement <clement0428@gmail.com>
2026-06-03 12:44:39 +0800

    Deploy O2O3 recognition matrix

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "bdf4e07",
-  deployed_at: "2026-06-03T12:15:30+08:00"
+  commit: "e050747",
+  deployed_at: "2026-06-03T12:41:56+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
