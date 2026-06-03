8f36b4f0c1ed91a3eb6662c363d6afba381d4b40
Clement <clement0428@gmail.com>
2026-06-03 11:01:57 +0800

    Deploy O2O3 recognition prefill

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "0986dcd",
-  deployed_at: "2026-06-03T18:46:00+08:00"
+  commit: "c64af31",
+  deployed_at: "2026-06-03T11:01:00+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
