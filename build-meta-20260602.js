03cd0c43f58130973a48b65c0d55eab3f182761a
Clement <clement0428@gmail.com>
2026-06-03 22:41:25 +0800

    Deploy O2O3 persisted pest uploads

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,6 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'e0d1457',
-  deployed_at: '2026-06-03T22:20:21+08:00',
+  commit: 'a739a23',
+  source_commit: 'a739a23b576f52bbd815437481ebe2591bcc53ad',
+  deployed_at: '2026-06-03T22:41:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
