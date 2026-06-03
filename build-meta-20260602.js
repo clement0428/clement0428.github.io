c4b75a60253f97185e16dd8229bc5bf7cf2a9d15
Clement <clement0428@gmail.com>
2026-06-04 07:43:49 +0800

    Deploy O2O3 weak recognition clarity

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'e9f7e12',
-  source_commit: 'e9f7e127fbdce2c7f0c3d3328ef49dba33ea562c',
-  deployed_at: '2026-06-03T22:55:00+08:00',
+  commit: '57e7eb8',
+  source_commit: '57e7eb8e6d1172e62961e34014728151b935a1c2',
+  deployed_at: '2026-06-04T00:12:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
