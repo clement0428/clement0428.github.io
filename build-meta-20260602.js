b08dfe477946fba7cc857a5db5c24ab5eee89e4e
Clement <clement0428@gmail.com>
2026-06-03 08:48:59 +0800

    Deploy obvious O2O3 photo upload

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,5 +1,5 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "29691fd",
-  deployed_at: "2026-06-03T16:55:00+08:00"
+  commit: "be6d4ad",
+  deployed_at: "2026-06-03T17:12:00+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
