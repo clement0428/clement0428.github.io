275c7fcf3e0d54a34e7fe826cda9781906f41a0c
Clement <clement0428@gmail.com>
2026-06-03 13:18:36 +0800

    Deploy O2O3 severity parsing fix

--- build-meta.js
+++ build-meta.js
@@ -1,5 +1,6 @@
 ﻿window.__WEGROW_ORBIT_BUILD__ = {
-  commit: "e050747",
-  deployed_at: "2026-06-03T12:41:56+08:00"
+  commit: "4f428cd",
+  deployed_at: "2026-06-03T13:17:12+08:00"
 };
 window.__WEGROW_BUILD_META__ = window.__WEGROW_ORBIT_BUILD__;
+
