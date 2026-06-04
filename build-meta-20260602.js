d82aaa1d5fcb252018c6832c36065ef7fbfa2f15
Clement <clement0428@gmail.com>
2026-06-04 12:51:21 +0800

    Deploy applied drip schedule verification state

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '018bdd8',
-  source_commit: '018bdd87d2aae60935c120cd30b7413befb00f67',
-  deployed_at: '2026-06-04T00:50:00+08:00',
+  commit: '6ccd327',
+  source_commit: '6ccd3271784e18c2db84e9ad7f1df18faff753ba',
+  deployed_at: '2026-06-04T12:49:23+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
--- build-meta.js
+++ build-meta.js
@@ -1,6 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'e0d1457',
-  deployed_at: '2026-06-03T22:20:21+08:00',
+  commit: '6ccd327',
+  source_commit: '6ccd3271784e18c2db84e9ad7f1df18faff753ba',
+  deployed_at: '2026-06-04T12:49:23+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
