bfe32035639f429ab2e58b94b774ba7c688f1ecf
Clement <clement0428@gmail.com>
2026-06-05 09:39:53 +0800

    Deploy expandable drainage archives

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '9675f52',
-  source_commit: '9675f5260f4a2ca31cff0db5af7165c5cac263ba',
-  deployed_at: '2026-06-05T09:09:56+08:00',
+  commit: 'edef192',
+  source_commit: 'edef1925a2af04dad69873a46178917f3af4b0a6',
+  deployed_at: '2026-06-05T09:38:23+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
