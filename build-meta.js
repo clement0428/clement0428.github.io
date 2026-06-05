4375fcd77366be97d23c80a22b694550b0d43fd2
Clement <clement0428@gmail.com>
2026-06-05 09:59:46 +0800

    Restore guard for expandable drainage archives

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
   commit: 'edef192',
   source_commit: 'edef1925a2af04dad69873a46178917f3af4b0a6',
-  deployed_at: '2026-06-05T09:38:23+08:00',
+  deployed_at: '2026-06-05T09:58:42+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
