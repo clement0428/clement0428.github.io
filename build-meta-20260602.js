b6d3d168e95aa57d15df68b1021aab95cc76fbff
Clement <clement0428@gmail.com>
2026-06-05 09:11:43 +0800

    Deploy drainage archive bundle

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '6ccd327',
-  source_commit: '6ccd3271784e18c2db84e9ad7f1df18faff753ba',
-  deployed_at: '2026-06-04T12:49:23+08:00',
+  commit: '9675f52',
+  source_commit: '9675f5260f4a2ca31cff0db5af7165c5cac263ba',
+  deployed_at: '2026-06-05T09:09:56+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
