fcbf90230bcf151d51c02b3859e33a8231b9378b
Clement <clement0428@gmail.com>
2026-06-08 09:07:42 +0800

    Deploy drainage R and seedling layout

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'd1004b1',
-  source_commit: 'd1004b14cd5860129040a7d86ff804c8fe3c4477',
-  deployed_at: '2026-06-06T07:58:36+08:00',
+  commit: 'c03a3c0',
+  source_commit: 'c03a3c0ff35dd05af53b7b099a9ca4008a8d6a26',
+  deployed_at: '2026-06-08T09:18:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
