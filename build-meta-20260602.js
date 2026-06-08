790596aeea7df19338fb8bb1dced31b461521f36
Clement <clement0428@gmail.com>
2026-06-08 14:30:09 +0800

    Deploy KengYi guard ledger proof

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'a05043f',
-  source_commit: 'a05043f65428a24d0d174558d691790218465789',
-  deployed_at: '2026-06-08T14:36:00+08:00',
+  commit: '23cbead',
+  source_commit: '23cbead11cd44363e63e5773a819559e3aac72a8',
+  deployed_at: '2026-06-08T14:49:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
