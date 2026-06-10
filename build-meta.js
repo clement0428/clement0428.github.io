b249f70940f7c6b12e9c01272e357e942ea8c8af
Clement <clement0428@gmail.com>
2026-06-10 14:13:29 +0800

    Deploy Hugreen access-layer QA gate

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,7 @@
 ﻿window.__WEGROW_BUILD_META__ = {
-  commit: '7b8fb06',
-  source_commit: '7b8fb0680d8c9931ec1f099c9cbe13899474089c',
-  deployed_at: '2026-06-10T08:45:07+08:00',
+  commit: 'a54896e',
+  source_commit: 'a54896ebfdcb8f8ba5785303b98827df57a42edb',
+  deployed_at: '2026-06-10T14:12:26+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
