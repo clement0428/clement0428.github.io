e51c209f883efdbc2e6d832840efc490366fbb17
Clement <clement0428@gmail.com>
2026-06-11 10:08:37 +0800

    Deploy Madou ABCDE pest plan visibility

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
-window.__WEGROW_BUILD_META__ = {
-  commit: '6284e64',
-  source_commit: '6284e64527c3c2abf1728e8a4c9271ba713323c5',
-  deployed_at: '2026-06-11T09:08:38+08:00',
+﻿window.__WEGROW_BUILD_META__ = {
+  commit: '7638bf1',
+  source_commit: '7638bf1159674fe8fdcfd75b5ac99384519ed258',
+  deployed_at: '2026-06-11T10:15:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked',
 };
