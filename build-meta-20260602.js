f94823fe39510123df0bb90792546a4c65dca067
Clement <clement0428@gmail.com>
2026-06-11 12:48:14 +0800

    Deploy manual pest slot protection

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 window.__WEGROW_BUILD_META__ = {
-  commit: 'a3e3970',
-  source_commit: 'a3e3970d378402616234a2283079a202748e6808',
-  deployed_at: '2026-06-11T12:36:55+08:00',
+  commit: 'b4a670c',
+  source_commit: 'b4a670cde8f8d920c5bc9505515392a348ad758e',
+  deployed_at: '2026-06-11T12:47:36+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked'
 };
