5a529982666782b39350e91a6f230891d51f8733
Clement <clement0428@gmail.com>
2026-06-11 10:16:36 +0800

    Fix source hash in build meta

--- build-meta.js
+++ build-meta.js
@@ -1,6 +1,6 @@
 ﻿window.__WEGROW_BUILD_META__ = {
   commit: 'd09ed68',
-  source_commit: 'd09ed682f1ff6e99e91754cfc703f1ff7fa10747',
+  source_commit: 'd09ed6886d30b0f27512054c9ad59a0339571ed1',
   deployed_at: '2026-06-11T10:25:00+08:00',
   frontend_bundle_deployed: 'pass',
   photo_storage_check: 'not_checked',
