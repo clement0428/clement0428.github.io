e6ba356d32c2ef404d94ee2addbb6a504b626d0a
Clement <clement0428@gmail.com>
2026-06-11 11:33:51 +0800

    Deploy Madou ABCDE pest plan f11cf8e

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,2 @@
-﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'd09ed68',
-  source_commit: 'd09ed6886d30b0f27512054c9ad59a0339571ed1',
-  deployed_at: '2026-06-11T10:25:00+08:00',
-  frontend_bundle_deployed: 'pass',
-  photo_storage_check: 'not_checked',
-};
+﻿window.__WEGROW_BUILD__ = { commit: 'f11cf8e', source_commit: 'f11cf8ea1f7a407197c48582344534b073c8ec64', deployed_at: '2026-06-11T11:34:47+08:00' };
+
