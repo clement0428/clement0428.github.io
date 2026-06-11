1da6d4572f27e56083c3b489813e93212274c0c5
Clement <clement0428@gmail.com>
2026-06-11 09:09:37 +0800

    Deploy product master completion

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
-﻿window.__WEGROW_BUILD_META__ = {
-  commit: 'a54896e',
-  source_commit: 'a54896ebfdcb8f8ba5785303b98827df57a42edb',
-  deployed_at: '2026-06-10T14:12:26+08:00',
+window.__WEGROW_BUILD_META__ = {
+  commit: '6284e64',
+  source_commit: '6284e64527c3c2abf1728e8a4c9271ba713323c5',
+  deployed_at: '2026-06-11T09:08:38+08:00',
   frontend_bundle_deployed: 'pass',
-  photo_storage_check: 'not_checked'
+  photo_storage_check: 'not_checked',
 };
