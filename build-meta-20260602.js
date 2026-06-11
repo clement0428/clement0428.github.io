401ddfc3727f1bfa9589b0574be078a052391df3
Clement <clement0428@gmail.com>
2026-06-11 12:38:08 +0800

    Deploy manual pest slot ownership memory

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
-﻿window.__WEGROW_BUILD_META__ = {
-  commit: '5d6ed08',
-  source_commit: '5d6ed0893733c4647b716d1d98602592374177f0',
-  deployed_at: '2026-06-11T12:00:51+08:00',
+window.__WEGROW_BUILD_META__ = {
+  commit: 'a3e3970',
+  source_commit: 'a3e3970d378402616234a2283079a202748e6808',
+  deployed_at: '2026-06-11T12:36:55+08:00',
   frontend_bundle_deployed: 'pass',
-  photo_storage_check: 'not_checked',
+  photo_storage_check: 'not_checked'
 };
