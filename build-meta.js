cf99860f851f84ff46c2df0ea4c35a5fb25b53fe
Clement <clement0428@gmail.com>
2026-06-25 01:51:55 +0800

    Deploy KengYi crop SOP cards

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,7 @@
-window.__WEGROW_BUILD_META__ = {
+﻿window.__WEGROW_BUILD_META__ = {
   commit: "eaae085",
   source_commit: "eaae0853806d811ea47c55acc83f2f456c8a6224",
-  deployed_at: "2026-06-24T17:44:45Z",
+  deployed_at: "2026-06-24T17:52:30Z",
   frontend_bundle_deployed: "pass",
   photo_storage_check: "not_checked"
 };
