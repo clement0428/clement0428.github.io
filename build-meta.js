db27729ae95e80e6e3467cce41d60fb04b44f4b9
Clement <clement0428@gmail.com>
2026-07-30 22:46:41 +0800

    Deploy product master mechanism codes

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,9 @@
 window.__WEGROW_BUILD_META__ = {
-  commit: "0364a19",
-  source_commit: "0364a19975d5d831056b723fc4cbac1ab43ab923",
-  deployed_at: "2026-07-30T12:54:20Z",
+  commit: "8ceb78b",
+  source_commit: "8ceb78b9f2ee21a0a2610a35868d70eec7213845",
+  deployed_at: "2026-07-30T14:46:25Z",
   frontend_bundle_deployed: "pass",
-  photo_storage_check: "not_checked"
+  photo_storage_check: "not_checked",
+  source: "manual-pages-deploy"
 };
+window.WEGROW_BUILD_META = window.__WEGROW_BUILD_META__;
