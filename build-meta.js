f6afd44a9eba191bca22c7a86348b8d4a2d0a913
Clement <clement0428@gmail.com>
2026-07-31 09:52:57 +0800

    Redeploy corrected pesticide mechanism codes

--- build-meta.js
+++ build-meta.js
@@ -1,7 +1,9 @@
 window.__WEGROW_BUILD_META__ = {
   commit: "5c48acc",
   source_commit: "5c48acc59ed5de48402b0b08b87946a633753e21",
-  deployed_at: "2026-07-31T01:49:29Z",
+  deployed_at: "2026-07-31T01:52:53Z",
   frontend_bundle_deployed: "pass",
-  photo_storage_check: "not_checked"
+  photo_storage_check: "not_checked",
+  source: "manual-pages-deploy"
 };
+window.WEGROW_BUILD_META = window.__WEGROW_BUILD_META__;
