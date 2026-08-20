d67099ba6fd547613e269610f8eb713777b2edec
Clement <clement0428@gmail.com>
2026-08-20 18:10:41 +0800

    Make legacy build meta non-overriding

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,4 +1,4 @@
-window.__WEGROW_BUILD_META__ = {
+window.__WEGROW_BUILD_META__ = window.__WEGROW_BUILD_META__ || {
   commit: "fdc8715",
   source_commit: "fdc871568d6250a2262aa26601088d38817e5215",
   deployed_at: "2026-08-20T09:38:51Z",
