f12423b8e45304c8f39c65a85ff3f51086f74274
Clement <clement0428@gmail.com>
2026-08-20 17:40:31 +0800

    Restore production guard scripts after deploy

--- build-meta-20260602.js
+++ build-meta-20260602.js
@@ -1,7 +1,7 @@
 window.__WEGROW_BUILD_META__ = {
   commit: "fdc8715",
   source_commit: "fdc871568d6250a2262aa26601088d38817e5215",
-  deployed_at: "2026-08-20T09:36:27Z",
+  deployed_at: "2026-08-20T09:38:51Z",
   frontend_bundle_deployed: "pass",
   photo_storage_check: "not_checked"
 };
